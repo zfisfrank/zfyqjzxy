04eb1ade71656f1f4687cf748fd86d154890d7f9
zfishere <zfishere@gmail.com>
2017-03-20 22:39:55 +0800

    deleted actor/director data

--- testModelGross.py
+++ testModelGross.py
@@ -20,7 +20,8 @@ interestNumCols = ['num_critic_for_reviews', 'duration',
        'aspect_ratio', 'movie_facebook_likes', 'budget_clean', ]
 
 # convert cate informations, each category will have either 0 or 1
-interestCatCols = ['language', 'country','content_rating','director_name','actor_1_name']
+# interestCatCols = ['language', 'country','content_rating','director_name','actor_1_name']
+interestCatCols = ['language', 'country','content_rating']
 # interestCatCols = ['language', 'country','content_rating']
 catInfo = fullData[interestCatCols]
 # le = []
